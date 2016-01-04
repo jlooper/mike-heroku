e98462f5148d9f71852377a376ce8bb5558006fb
jlooper <jen.looper@gmail.com>
2016-01-03 22:36:10 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var contacts = require('./routes/contact');
+
+var app = express();
+
+var dbName = 'contacts';
+//for prod
+var connectionString = process.env.MONGO_DB;
+//for test
+//var connectionString = 'place your connection string here as defined on Modulus';
+mongoose.connect(connectionString);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api',contacts);
+
+//register our engine as .html so we can have .html pages
+app.engine('.html', require('ejs').__express);
+
+//set our view engine to HTML
+app.set('view engine', 'html');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
